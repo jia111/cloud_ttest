961d67f6159ba3c19de7b8d761a58a8ef7dac080
liaojiajia <liaojiajia@snqu.com>
2024-08-28 12:08:50 +0800

    feat:1.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+const nextConfig = {
+  devServer: {
+    https: true,
+    ignoreCertificateErrors: true, // 忽略证书错误
+  },
+};
 
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
-
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform();
+// }
 export default nextConfig;
