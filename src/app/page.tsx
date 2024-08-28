523ff232db2dd6395de635450bc5b73e71ed8586
liaojiajia <liaojiajia@snqu.com>
2024-08-28 12:52:40 +0800

    getStaticProps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react'
 import styles from './page.module.css'
 export const runtime = "edge";
-export default async function Home() {
-  const res = await fetch(`http://w2a.auth.la/info`)
-  const resData: any = await res.json()
-  const { data } = resData
+export default async function Home({ data }) {
+
   if (!data) {
     return <>还没有数据哟</>
   }
@@ -16,4 +14,18 @@ export default async function Home() {
       {data?.screenshot?.map((item: any, i: number) => <img key={i} src={item.src} />)}
     </>
   )
+}
+export async function getStaticProps(context) {
+  const res = await fetch(`http://w2a.auth.la/info`)
+  const data = await res.json()
+
+  if (!data.data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data: data.data }, // will be passed to the page component as props
+  }
 }
