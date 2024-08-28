f8777697c2010ede0ac09e6fe92b8ce4b6568372
liaojiajia <liaojiajia@snqu.com>
2024-08-28 14:00:56 +0800

    12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,23 @@
 
 
-// 'use client'
+'use client'
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
-export const runtime = "edge";
-export default async function Home() {
-  let posts: any
-  try {
+// export const runtime = "edge";
+// export default async function Home() {
+//   let posts: any
+//   try {
 
-    const res = await fetch(`http://w2a.auth.la/info`)
-    const resData: any = await res.json()
-    posts = resData.data
-  } catch (error) {
+//     const res = await fetch(`http://w2a.auth.la/info`)
+//     const resData: any = await res.json()
+//     posts = resData.data
+//   } catch (error) {
 
-  }
-  if (!posts) {
-    return <>还没有数据哟</>
-  }
-  return (
-    <>
-      <div>名字：{posts?.name}</div>
-      <div style={{ position: 'relative', height: 192, width: 192 }}>ICON：<Image src={posts?.desktop_icon?.src} alt="Icon" layout="fill" /></div>
-      <div>图片：</div>
-      {posts?.screenshot?.map((item: any, i: number) => <div style={{ position: 'relative', height: 587, width: 330 }}> <Image key={i} src={item?.src} alt="Icon" layout="fill" /></div>)}
-    </>
-  )
-}
-
-
-
-// export default function Home() {
-//   const [posts, setPosts] = useState<any>(null)
-
-//   useEffect(() => {
-//     async function fetchPosts() {
-//       const res = await fetch(`https://w2a.auth.la/info`)
-//       let data: any = await res.json()
-//       setPosts(data?.data)
-//     }
-//     fetchPosts()
-//   }, [])
-
-//   if (!posts) return <div>Loading...</div>
-
+//   }
+//   if (!posts) {
+//     return <>还没有数据哟</>
+//   }
 //   return (
 //     <>
 //       <div>名字：{posts?.name}</div>
@@ -53,3 +27,29 @@ export default async function Home() {
 //     </>
 //   )
 // }
+
+
+
+export default function Home() {
+  const [posts, setPosts] = useState<any>(null)
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const res = await fetch(`https://w2a.auth.la/info`)
+      let data: any = await res.json()
+      setPosts(data?.data)
+    }
+    fetchPosts()
+  }, [])
+
+  if (!posts) return <div>Loading...</div>
+
+  return (
+    <>
+      <div>名字：{posts?.name}</div>
+      <div style={{ position: 'relative', height: 192, width: 192 }}>ICON：<Image src={posts?.desktop_icon?.src} alt="Icon" layout="fill" /></div>
+      <div>图片：</div>
+      {posts?.screenshot?.map((item: any, i: number) => <div style={{ position: 'relative', height: 587, width: 330 }}> <Image key={i} src={item?.src} alt="Icon" layout="fill" /></div>)}
+    </>
+  )
+}
