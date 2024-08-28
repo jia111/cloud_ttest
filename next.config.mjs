c58d459a54d85dad34fc25c02c3e686c72888dce
liaojiajia <liaojiajia@snqu.com>
2024-08-28 12:33:38 +0800

    feat:11

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-const nextConfig = {
-  devServer: {
-    https: true,
-    ignoreCertificateErrors: true, // 忽略证书错误
-  },
-};
 
-// if (process.env.NODE_ENV === 'development') {
-//   await setupDevPlatform();
-// }
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+// (when running the application with `next dev`), for more information see:
+// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 export default nextConfig;
