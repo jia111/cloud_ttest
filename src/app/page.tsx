f2764d6f2574e3990b5ee0830ec99f49fba48233
liaojiajia <liaojiajia@snqu.com>
2024-08-28 12:55:41 +0800

    55

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,19 @@
 import { useEffect } from 'react'
 import styles from './page.module.css'
 export const runtime = "edge";
-export default async function Home({ data }) {
+export default async function Home() {
+  const res = await fetch(`http://w2a.auth.la/info`)
+  const resData = await res.json()
 
-  if (!data) {
+  if (!resData?.data) {
     return <>还没有数据哟</>
   }
   return (
     <>
-      <div>名字：{data?.name}</div>
-      <div>ICON：<img src={data?.desktop_icon?.src} /></div>
+      <div>名字：{resData?.data?.name}</div>
+      <div>ICON：<img src={resData?.data?.desktop_icon?.src} /></div>
       <div>图片：</div>
-      {data?.screenshot?.map((item: any, i: number) => <img key={i} src={item.src} />)}
+      {resData?.data?.screenshot?.map((item: any, i: number) => <img key={i} src={item.src} />)}
     </>
   )
-}
-export async function getStaticProps(context) {
-  const res = await fetch(`http://w2a.auth.la/info`)
-  const data = await res.json()
-
-  if (!data.data) {
-    return {
-      notFound: true,
-    }
-  }
-
-  return {
-    props: { data: data.data }, // will be passed to the page component as props
-  }
 }
