5f7983017c99fdadb4ce75680e6c1b6ad20692b6
liaojiajia <liaojiajia@snqu.com>
2024-08-29 10:45:11 +0800

    14

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import {
+  setupDevPlatform
+} from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {  images: {
-  remotePatterns: [
-    {
+const nextConfig = {
+  images: {
+    remotePatterns: [{
       protocol: 'https',
       hostname: 'static.w2.app',
       port: '',
-      pathname: '/20240627/**',
-    },
-  ],
-},};
+    }, ],
+  },
+};
 
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
