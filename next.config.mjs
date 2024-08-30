50db503b4542986091b1c962c5295a6887a32b99
liaojiajia <liaojiajia@snqu.com>
2024-08-30 16:17:13 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import {
-  setupDevPlatform
-} from '@cloudflare/next-on-pages/next-dev';
+// import {
+//   setupDevPlatform
+// } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,8 +13,8 @@ const nextConfig = {
   },
 };
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform();
+// }
 
 export default nextConfig;
